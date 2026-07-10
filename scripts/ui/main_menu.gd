369622957de451339ae503ebf3cf29e647328094
KenzYo1 <kenzoyo123@gmail.com>
2026-07-10 20:37:21 +0700

    audio
    baru main menu karena audio lain ga bisa diimplement tanpa level

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -2,22 +2,19 @@ extends Node
 
 @export var button_array: Array[TextureButton]
 @export var settings: Node
-@export var BGM: AudioStream # temporary, aku mau denger lagu claris
+@export var BGMPath: String
+@export var ClickSound: String
 
 # 0 = play, 1 = settings, 2 = quit
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# temporary, aku mau denger lagu claris
-	var temp_music_player: AudioStreamPlayer = AudioStreamPlayer.new()
-	add_child(temp_music_player)
-	temp_music_player.stream = BGM
-	temp_music_player.play()
-	
+	AudioManager.play_bgm(BGMPath)
 	for i in range(len(button_array)):
 		button_array[i].pivot_offset = button_array[i].size / 2
 		button_array[i].pressed.connect(_on_button_pressed.bind(i))
 
 func _on_button_pressed(idx: int) -> void:
+	AudioManager.play_sfx(ClickSound)
 	match idx:
 		0: get_tree().change_scene_to_file("res://scenes/level_selection.tscn")
 		1: UiAnimManager.moveDownAnim(settings, Vector2.ZERO, 0.3)
