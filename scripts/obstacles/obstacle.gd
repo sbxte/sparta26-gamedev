ac8b0bb9348eae9284b2d7a5c5fb597f08510ee9
SByte <stormybytes@gmail.com>
2026-07-10 21:15:53 +0700

    0.01 editor step size for base spawn chance

--- scripts/obstacles/obstacle.gd
+++ scripts/obstacles/obstacle.gd
@@ -3,7 +3,7 @@ class_name Obstacle
 extends Node2D
 
 @export_group("Spawning")
-@export_range(0.0, 1.0, 0.1) var base_spawn_chance := 0.5
+@export_range(0.0, 1.0, 0.01) var base_spawn_chance := 0.5
 
 @abstract
 func setup(ses: Session, chance: float) -> void
